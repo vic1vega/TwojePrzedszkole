ff0cf6fc445b1bc4f967f9786898858bf621acec
vic1vega <janusz.pacholski@outlook.com>
2019-09-04 09:11:16 +0200

    Add Users Table to DB

--- TwojePrzedszkole.API/Data/DataContext.cs
+++ TwojePrzedszkole.API/Data/DataContext.cs
@@ -7,7 +7,9 @@ namespace TwojePrzedszkole.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
-        public DbSet<Value> Values { get; set; }
+        public DbSet<Value> Values { get; set; }        
+        public DbSet<User> Users { get; set; }        
+
         
     }
 }
